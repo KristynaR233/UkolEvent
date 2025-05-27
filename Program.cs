755ec777baa45f68dbaf727f49651ca6104ba99c
KristynaR233 <randova.kristyna@seznam.cz>
2025-05-27 21:50:48 +0200

    switch a vstup

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+using System;
 using System.ComponentModel;
+using System.Diagnostics.Tracing;
 using System.Dynamic;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Event.Ukol;
 
@@ -22,15 +24,110 @@ class Program
             int month = int.Parse(poleDat[1]);
             int day = int.Parse(poleDat[2]);
             DatumUdalosti = new DateTime(year, month, day);
+        }
+    
+
+        public void List()
+        {
+            TimeSpan dny = DatumUdalosti.Date - DateTime.Now.Date;
+            Console.WriteLine($"Event {Jmeno} with date {DatumUdalosti} will happen in {dny.Days} days");
+        }
+
+
+
+        public void Stats()
+        {
+            Console.WriteLine($"Date:{DatumUdalosti.Date}: events:");
+        }
+
+        public static string NactiUdalostOdUzivatele()
+        {
+            Console.WriteLine("Uzivateli zadej název a datum udalosti ve formátu: EVENT;jmeno udalosti ;datum ve formatu yyyy-mm-dd");
+            string vstupUzivatele = Console.ReadLine();
+            return vstupUzivatele;
+           
+           
             
 
-
         }
 
-        public void VypisEvent()
+
+        static void Main(string[] args)
         {
-            Console.WriteLine($"Event:{Jmeno}; {DatumUdalosti.Date}");
-        }
+
+            /*Console.WriteLine("Uzivateli zadej název a datum udalosti ve formátu: EVENT;jmeno udalosti ;datum ve formatu yyyy-mm-dd");
+            string vstupUzivatele = Console.ReadLine();*/
+            
+
+            
+            Event lekce = new Event("Lekce Czechitas", "2025-05-29");
+            Event koncert = new Event("Koncert", "2025-05-28");
+           // var newEvent = new Event(jmeno, datum);
+           List<Event> eventsList = new List<Event>()
+        {   lekce,
+            koncert
+
+        };
+            foreach (Event name in eventsList)
+            {
+                // name.VypisEvent();
+            }
+
+
+
+
+            Dictionary<string, int> udalosti = new Dictionary<string, int>
+            {
+
+
+            };
+
+            while (true)
+            {
+                Console.WriteLine("1 - ulozit novou událost");
+                Console.WriteLine("2 - vypsat udalosti");
+                Console.WriteLine("3 - vypsat datum a pocet udalosti");
+                Console.WriteLine("4 - ukoncit aplickaci");
+                Console.WriteLine("Uzivateli, zvol akci:");
+                int akce = int.Parse(Console.ReadLine());
+
+                switch (akce)
+                {
+                    case 1:
+                    string[] poleVstupu = NactiUdalostOdUzivatele().Split(";");
+                    string jmeno = poleVstupu[1];
+                    string datum = poleVstupu[2];
+                    var newEvent = new Event(jmeno, datum);
+                    eventsList.Add(newEvent);
+                        break;
+                        
+                    case 2:
+                        foreach (Event name in eventsList)
+                        {
+                            name.List();
+                        }
+                        break;
+                    case 3:
+                        foreach (Event name in eventsList)
+                        {
+                            name.Stats();
+                        }
+                        break;
+                    case 4:
+                        Console.WriteLine("Ukoncuji apikaci");
+                        return;
+
+
+                }
+
+            }
+
+
+        }// endMain      
+    }
+}// endProgram
+
+
 
         public void List()
         {
